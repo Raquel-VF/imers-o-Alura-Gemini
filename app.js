1016110fb4a20ad1b5c3f98ed93a7deebb3f5fb9
raquel <raquelvargas0905@gmail.com>
2024-09-11 22:38:21 -0300

    feito o "frame"  signo-corpo

--- app.js
+++ app.js
@@ -48,3 +48,77 @@ function calcularSigno() {
     document.querySelector(".descricao").innerText = descricao;
 
 }
+
+function mostrarSigno(signo) {
+    const signoData = {
+        aries: {
+            nome: "Áries",
+            partesCorpo: "cabeça, crânio, couro cabeludo, rosto",
+            desafiosSaude: "enxaquecas, dores de cabeça, problemas nos ouvidos e nariz."
+        },
+        touro: {
+            nome: "Touro",
+            partesCorpo: "toda a região do pescoço e garganta. Abrange a boca, língua, laringe e cordas vocais. A glândula taurina é a tireoide.",
+            desafiosSaude: "pessoas com destaque em Touro podem ter vulnerabilidade na garganta, com mais tendência a infecções e problemas diversos. Tem potencial para hipo ou hiper tireoidíssimo quando há desequilíbrios, bem como o crescimento anormal da tireoide, resultando em bócio."
+        },
+        gemeos: {
+            nome: "Gêmeos",
+            partesCorpo: "este versátil signo rege todo o aparelho respiratório, pulmões, ombros, braços e mãos.",
+            desafiosSaude: " todas as afecções respiratórias são governadas por Gêmeos, como a asma ou a bronquite. Algumas pessoas com Gêmeos destacado podem ter maior vulnerabilidade a gripes e resfriados. Os problemas nos ombros, braços e mãos, como tendinite, bursite e lesões por esforço repetitivo, também podem acontecer com quem tem este signo em destaque."
+        },
+        cancer: {
+            nome: "Câncer",
+            partesCorpo: "a região do ventre, notadamente o estômago, bem como os seios e as glândulas mamárias. O esôfago também está sob a sua regência.",
+            desafiosSaude: "os empáticos cancerianos, regidos pela Lua, são famosos por terem o estômago mais sensível, com mais tendência a azias e úlceras estomacais. O útero também é regido por Câncer (mas não os ovários.) Tem tendência a inchaços e retenção de líquidos."
+        },
+        leao: {
+            nome: "Leão",
+            partesCorpo: " à medida em que se tornam mais velhos, pessoas com Leão em destaque no mapa precisam aprender a moderar o temperamento forte e também os prazeres à mesa para que não sejam entupidas as veias e artérias com excesso de gordura, podendo gerar problemas cardíacos."
+        },
+        virgem: {
+            nome: "Virgem",
+            partesCorpo: "o meticuloso signo de Virgem tem regência sobre os intestinos, em especial o delgado.",
+            desafiosSaude: "Virgem o signo que rege a alimentação, e a pessoa virginiana não demora a perceber que precisa ser mais seletivo e saudável para se alimentar para não ter problemas como colites, cólicas intestinais e disfunções gástricas, também mais comuns quando se torna muito nervoso e perfeccionista."
+        },
+        libra: {
+            nome: "Libra",
+            partesCorpo: "o clássico signo de Libra rege a região da cintura e também os rins, com grande sensibilidade neste par de órgãos.",
+            desafiosSaude: " mais do que qualquer outro, precisa observar a adequada ingestão de líquidos. Infecções urinárias, por exemplo, podem levar a inflamação nos rins, que se não tratada podem ser fatal."
+        },
+        escorpiao: {
+            nome: "Escorpião",
+            partesCorpo: " o intenso signo de Escorpião tem regência sobre a bexiga e órgãos sexuais e reprodutores, como pênis, próstata, vagina, ânus e, também, sobre o intestino grosso.",
+            desafiosSaude: "não é incomum que pessoas com Escorpião em destaque possam ter as dolorosas hemorroidas. Precisa ter especial cuidado com doenças sexualmente transmissíveis."
+        },
+        sagitario: {
+            nome: "Sagitário",
+            partesCorpo: "o otimista signo de Sagitário, governado por Júpiter, rege o quadril, o fêmur e as coxas, bem como o fígado e o pâncreas.",
+            desafiosSaude: "um estilo desregrado de vida pode levar pessoas com destaque no signo de Sagitário a problemas no fígado e também com sobrepeso. Há potencial também de em movimentos de crise quebrarem a bacia ou o fêmur."
+        },
+        capricornio: {
+            nome: "Capricórnio",
+            partesCorpo: "o focado signo de Capricórnio ter regência sobre as partes duras do corpo, como dentes, ossos e joelhos.",
+            desafiosSaude: " não é incomum pessoas com forte destaque de Capricórnio ou de Saturno, o seu regente, tenham tratamentos dentários prolongados para realizar uma recorrência neste tipo de questão. Abrange todas as questões ortopédicas e de reumatismo."
+        },
+        aquario: {
+            nome: "Aquário",
+            partesCorpo: " o agitado signo de Aquário a região do tornozelo e calcanhares, bem como a circulação sanguínea.",
+            desafiosSaude: "Pessoas com o signo de Aquário em destaque quando estão em desequilíbrio podem ter problemas de pressão alta ou varizes. Aquário ou o seu regente, Urano, em destaque também pode ocasionar propensão a acidentes."
+        },
+        peixes: {
+            nome: "Peixes",
+            partesCorpo: "o sistema linfático é governando pelo sensível signo de Peixes, que também rege os pés, ficando mais vulneráveis a fascite plantar e deformidades nos dedos.",
+            desafiosSaude: "em função de grande sensibilidade, pessoas com Peixes em destaque no mapa podem ter doenças de difícil diagnóstico, além de sintomas alérgicos. São especialmente vulneráveis, bem como quem tem Netuno, o regente de Peixes, em destaque, a todo tipo de química, medicamento, álcool e drogas, precisando tomar cuidado com isto. Atribui-se a glândula pineal a Peixes em razão da conexão dela com processos de meditação e conexão espiritual."
+        },
+    }
+    
+    const {nome, partesCorpo, desafiosSaude} = signoData[signo];
+
+    document.querySelector(".icones-signos-iframe").innerHTML = `
+        <div class="signo-parte-corpo">
+            <h2>${nome}</h2>
+            <p><strong>Partes do corpo que rege:</strong> ${partesCorpo}</p>
+            <p><strong>Possíveis desafios na saúde:</strong> ${desafiosSaude}</p>
+        </div>
+    `
+}
